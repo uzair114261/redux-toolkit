e08c9ae56c481b78ee8d6f3128cc10db9ca55a8b
Muhammad Uzair <rajputuzair58@gmail.com>
2024-04-30 18:25:02 +0500

    Setup a store for redux and created two slices

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import {Layout, Typography, Space} from 'antd'
+function App() {
+  const dispatch = useDispatch()
+  const counter = useSelector((state) => state.counter.value)
+
+  return (
+    <>
+   
+    </>
+  )
+}
+
+export default App
